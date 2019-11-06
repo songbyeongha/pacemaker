9b56590cf59648630d5e889f7bd86a50c17028b9
TaeJuneJoung <jtj0525@gmail.com>
2019-11-06 09:16:13 +0900

    Local test

--- front/api/achieve.js
+++ front/api/achieve.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const config = {
-  baseUrl: 'http://13.124.241.124:8080/api/v1/usersAchieve'
+  baseUrl: 'http://localhost:8080/api/v1/usersAchieve'
 }
 
 /**
--- front/api/roomAdd.js
+++ front/api/roomAdd.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const config = {
-  baseUrl: 'http://13.124.241.124:8080/api/v1/'
+  baseUrl: 'http://localhost:8080/api/v1/'
 }
 
 function createRoom(roomData) {
